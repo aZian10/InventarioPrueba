9fc97493e69e23476bd3ab88fd2697bc6fbaecfc
aZian10 <161075880+aZian10@users.noreply.github.com>
2026-03-30 18:29:58 -0500

    Add files via upload

--- inventario.py
+++ inventario.py
@@ -1,35 +1,61 @@
-def agregar_producto(nombre, precio, cantidad):
-    #Pide el nombre del producto al usuario
-    nombre=str(input("Ingrese un nombre: "))
+#Lista para guardar inventario
+inventario=[]
+
+def agregar_producto():
     #Veficar que los datos correspondan
-    while True:
-        try:
-            precio=float(input("Ingrese el precio: "))
-            if precio<=0:
-                raise ValueError
-            break
-        except ValueError:
-            print("Ingrese un dato valido")
-    while True:
-        try:
-            cantidad=int(input("Ingrese cantidad: "))
-            if cantidad<=0:
-                raise ValueError
-            break
-        except ValueError:
-            print("Ingrese un dato valido")
-        #Guardamos el total
-        costo_total=precio*cantidad
-        #Muestra el resultado en consola
-    return nombre, precio, cantidad 
+    try:
+        nombre=input("Ingrese un nombre: ")
+        precio=float(input("Ingrese el precio: "))
+        cantidad=int(input("Ingrese cantidad: "))
+        producto={"nombre":nombre, "precio":precio, "cantidad":cantidad}
+        inventario.append(producto)
+
+    except ValueError:
+        print("Ingrese datos validos")
 
 def mostrar_inventario():
-    print("-----------------------------------")
-    print(f"Nombre: {nombre}\n Precio: {precio}\n Cantidad: {cantidad}\n Costo total: {costo_total}")
-    print("-----------------------------------")
-    return
+    if not inventario:
+        print("El inventario esta vacio")
+    else:
+        print("Inventario")
+        for i in inventario:
+            print(f"Producto: {i['nombre']} | Precio: {i['precio']:.2f} | Cantidad: {i['cantidad']}")
 
 def calcular_estadisticas():
-    return
+    print("Estadisticas")
+    if not inventario:
+        print("No hay datos")
+        return
+    
+    valor_total=sum(i['precio']* i['cantidad'] for i in inventario)
+    total_unidades=sum(i['cantidad'] for i in inventario)
+
+    print(f"Valor total: {valor_total:.2f}")
+    print(f"Cantidad total: {total_unidades}")
+
+def menu():
+    while True:
+        print("Sistema de gestion")
+        print("1. Agregar producto")
+        print("2. Mostrar inventario")
+        print("3. Calcular estadisticas")
+        print("4. Salir")
+
+        opcion=input("Seleccione una opcion:")
+
+        if opcion=="1":
+            agregar_producto()
+        elif opcion=="2":
+            mostrar_inventario()
+        elif opcion=="3":
+            calcular_estadisticas()
+        elif opcion=="4":
+            print("Salir")
+            break
+        else:
+            print("Opcion invalida")
+
+if __name__=="__main__":
+    menu()
 
     
